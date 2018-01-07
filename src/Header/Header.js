e18a3e839bdc8efebd01c08aaaafc415a5faa67f
Sarah Yu <sarahyu0692@gmail.com>
2018-01-06 23:51:31 -0500

    create header component

---
+++ src/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import './Header.css'
+
+const Header = () => (
+	<header>
+		<nav>
+			<a href="/" className="logo">
+				piece peace
+			</a>
+			<a href="/">home</a>
+			<a href="/boards">boards</a>
+		</nav>
+	</header>
+)
+
+export default Header
